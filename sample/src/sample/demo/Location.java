38bb824cebedc15d28e82a30604d33133ae1653b
Hafis Suhaimi <byhafxz@mydevs.co>
2021-03-18 21:16:08 +0800

    Second Location Commit

--- sample/src/sample/demo/Location.java
+++ sample/src/sample/demo/Location.java
@@ -3,6 +3,8 @@ package sample.demo;
 public class Location {
 	 private String name;
 	 private int locationID;
+	 private int address;
+	 private int housenumber;
 	 
 	 public String getName() {
 	        return name;
